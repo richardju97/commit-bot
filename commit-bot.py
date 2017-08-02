62a83ad927caa04bf65aad9b0b7092dd83f1249f
rju <rju@purdue.edu>
2017-08-02 15:12:05 -0700

    Writes commit time to file

--- commit-bot.py
+++ commit-bot.py
@@ -1,9 +1,13 @@
 # commit-bot.py
 
 import subprocess
+import datetime
+
+#print(datetime.datetime.now())
 
 file = open("myfile.txt", 'a')
-file.write("Test 2")
+file.write("Commit on " + str(datetime.datetime.now()))
+file.write("\n")
 file.close()
 
 #print(subprocess.check_output(['ls', '-l']))
