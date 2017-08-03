f9370f57c76c535f4e2702b4f0062938dc792b12
rju <rju@purdue.edu>
2017-08-03 00:00:25 -0700

    Can perform tasks at specific times

--- commit-bot.py
+++ commit-bot.py
@@ -2,13 +2,24 @@
 
 import subprocess
 import datetime
+from datetime import date
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+s = BlockingScheduler()
+
+def myFunction():
+    file = open("myfile.txt", 'a')
+    file.write("Commit on " + str(datetime.datetime.now()))
+    file.write("\n")
+    file.close()
+
+s.add_job(myFunction, 'date', run_date=datetime.datetime(2017, 8, 2, 23, 59, 50))
 
 #print(datetime.datetime.now())
 
-file = open("myfile.txt", 'a')
-file.write("Commit on " + str(datetime.datetime.now()))
-file.write("\n")
-file.close()
+
 
 #print(subprocess.check_output(['ls', '-l']))
 
+s.start()
+#s.print_jobs()
