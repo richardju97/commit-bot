25535f14aaa6b51b4bd2227ec6dbea930a29ab66
rju <rju@purdue.edu>
2017-08-03 00:19:18 -0700

    Date parameter can be incremented

--- commit-bot.py
+++ commit-bot.py
@@ -2,22 +2,24 @@
 
 import subprocess
 import datetime
-from datetime import date
+#from datetime import date
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 s = BlockingScheduler()
 
-def myFunction():
+def write():
     file = open("myfile.txt", 'a')
     file.write("Commit on " + str(datetime.datetime.now()))
     file.write("\n")
     file.close()
 
-s.add_job(myFunction, 'date', run_date=datetime.datetime(2017, 8, 2, 23, 59, 50))
+d = datetime.datetime(2017, 8, 3, 00, 16, 00)
+s.add_job(write, 'date', run_date=d)
 
 #print(datetime.datetime.now())
 
-
+d += datetime.timedelta(seconds=10)
+s.add_job(write, 'date', run_date=d)
 
 #print(subprocess.check_output(['ls', '-l']))
 
